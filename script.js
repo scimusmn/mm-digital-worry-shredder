3ffe1cafad7a4c4bcda4a684b2f09f4103f894cc
Sarah German <sgerman@pantheon.io>
2020-12-28 15:15:34 -0600

    Copy user text onto a card.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// alert('hello')
+let form = document.querySelector("form");
+let step2 = document.querySelector(".step2");
+
+document.querySelector("button").addEventListener("click", () => {
+    let worry = document.querySelector("textarea").value;
+    let worryCard = document.querySelector(".worry-card-user-text");
+
+    worryCard.append(worry);
+
+    // @todo require a value before moving on
+    form.style.display = "none";
+    step2.style.display = "inline-block";
+});
