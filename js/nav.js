2e997bb18b5d19b3c862febad570420993407020
Sarah German <sgerman@pantheon.io>
2021-02-13 16:38:04 -0600

    Remove unneeded variables (we only use these once)

--- js/nav.js
+++ js/nav.js
@@ -7,12 +7,10 @@
 
 // Screen 1 -> Screen 2
 document.querySelector('button').addEventListener('click', () => {
-  let worry = document.querySelector('textarea').value;
-  let worryCard = document.querySelector('#worry-card');
 
-  worryCard.insertAdjacentHTML(
+  document.querySelector('#worry-card').insertAdjacentHTML(
     'beforeend',
-    '<div class="worry-card-user-text">' + worry + '</div>',
+    '<div class="worry-card-user-text">' + document.querySelector('textarea').value + '</div>',
   );
 
   let tl = gsap.timeline();
